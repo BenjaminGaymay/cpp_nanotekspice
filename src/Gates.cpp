d2ba40e45826f21e0c2c1a6aebfd3565ba4993b4
Benjamin GAYMAY <benjamin.gaymay@epitech.eu>
2018-02-23 16:55:20 +0100

    undo

--- src/Gates.cpp
+++ src/Gates.cpp
@@ -7,32 +7,34 @@
 
 #include "Component.hpp"
 
-nts::Tristate gate_and(nts::Pin *input1, nts::Pin *input2)
-{
-	std::cout << "JE FAIS UN AND de " << input1->_component << ":" << input1->_index << " et " << input1->_component << ":" << input2->_index << std::endl;
-	return (input1->_state == TRUE && input2->_state == TRUE ? TRUE : FALSE);
-}
+namespace nts {
+	Tristate gate_and(Pin *input1, Pin *input2)
+	{
+		std::cout << "JE FAIS UN AND de " << input1->_component << ":" << input1->_index << " et " << input1->_component << ":" << input2->_index << std::endl;
+		return (input1->_state == TRUE && input2->_state == TRUE ? TRUE : FALSE);
+	}
 
-nts::Tristate gate_nand(nts::Pin *input1, nts::Pin *input2)
-{
-	std::cout << "JE FAIS UN NAND" << std::endl;
-	return (input1->_state == input2->_state ? TRUE : FALSE);
-}
+	Tristate gate_nand(Pin *input1, Pin *input2)
+	{
+		std::cout << "JE FAIS UN NAND" << std::endl;
+		return (input1->_state == input2->_state ? TRUE : FALSE);
+	}
 
-nts::Tristate gate_or(nts::Pin *input1, nts::Pin *input2)
-{
-	std::cout << "JE FAIS UN OR" << std::endl;
-	return (input1->_state == input2->_state ? TRUE : FALSE);
-}
+	Tristate gate_or(Pin *input1, Pin *input2)
+	{
+		std::cout << "JE FAIS UN OR" << std::endl;
+		return (input1->_state == input2->_state ? TRUE : FALSE);
+	}
 
-nts::Tristate gate_nor(nts::Pin *input1, nts::Pin *input2)
-{
-	std::cout << "JE FAIS UN NOR" << std::endl;
-	return (input1->_state == input2->_state ? TRUE : FALSE);
-}
+	Tristate gate_nor(Pin *input1, Pin *input2)
+	{
+		std::cout << "JE FAIS UN NOR" << std::endl;
+		return (input1->_state == input2->_state ? TRUE : FALSE);
+	}
 
-nts::Tristate get_output(nts::Pin *input, nts::Pin *useless)
-{
-	std::cout << "GET OUTPUT" << std::endl;
-	return (input->_state);
+	Tristate get_output(Pin *input, Pin *useless)
+	{
+		std::cout << "GET OUTPUT" << std::endl;
+		return (input->_state);
+	}
 }
