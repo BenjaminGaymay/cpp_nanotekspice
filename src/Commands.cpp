f7c88082733e4a5fb11a8eca33f191a291348d6d
Benjamin GAYMAY <benjamin.gaymay@epitech.eu>
2018-02-26 15:20:57 +0100

    add clock in simulate

--- src/Commands.cpp
+++ src/Commands.cpp
@@ -15,14 +15,14 @@
 
 bool g_quitLoop = false;
 
-void my_exit(const std::string &cmd, std::map<std::string, nts::Component *> cList)
+int my_exit(const std::string &cmd, std::map<std::string, nts::Component *> cList)
 {
 	(void)cmd;
 	(void)cList;
 	exit(0);
 }
 
-void display(const std::string &cmd, std::map<std::string, nts::Component *> cList)
+int display(const std::string &cmd, std::map<std::string, nts::Component *> cList)
 {
 	nts::Component *component;
 	nts::Pin *pin;
@@ -39,34 +39,48 @@ void display(const std::string &cmd, std::map<std::string, nts::Component *> cLi
 				std::cout << pin->_state << std::endl;
 		}
 	}
+	return 0;
 }
 
-void simulate(const std::string &cmd, std::map<std::string, nts::Component *> cList)
+int simulate(const std::string &cmd, std::map<std::string, nts::Component *> cList)
 {
 	nts::Component *component;
 	(void)cmd;
 
 	for (auto &c : cList) {
 		component = c.second;
+		if ((component->_type == "input" or component->_type == "clock")
+			and component->_pins[0]->_state == nts::UNDEFINED)
+			return std::cerr << "Error: " << component->_name << ":1 is undefined" << std::endl, 84;
+	}
+
+	for (auto &c : cList) {
+		component = c.second;
 		if (component->_type == "output")
 			component->refreshPinById(1, cList);
+		else if (component->_type == "clock")
+			component->_pins[0]->_state = (component->_pins[0]->_state == nts::TRUE ? nts::FALSE : nts::TRUE);
 	}
+	return 0;
 }
 
-void loop(const std::string &cmd, std::map<std::string, nts::Component *> cList)
+int loop(const std::string &cmd, std::map<std::string, nts::Component *> cList)
 {
 	(void)cmd;
 	while (! g_quitLoop) {
-		simulate(cmd, cList);
+		if (simulate(cmd, cList) == 84)
+			return 84;
 		usleep(500);
 	}
+	return 0;
 }
 
-void dump(const std::string &cmd, std::map<std::string, nts::Component *> cList)
+int dump(const std::string &cmd, std::map<std::string, nts::Component *> cList)
 {
 	(void)cmd;
 	for (auto &c : cList)
 		c.second->dump();
+	return 0;
 }
 
 void quitLoop(int empty)
@@ -75,7 +89,7 @@ void quitLoop(int empty)
 	g_quitLoop = true;
 }
 
-void createSignal()
+int createSignal()
 {
 	struct sigaction sigIntHandler;
 
@@ -84,11 +98,12 @@ void createSignal()
 	sigIntHandler.sa_flags = 0;
 	sigaction(SIGINT, &sigIntHandler, NULL);
 	g_quitLoop = false;
+	return 0;
 }
 
-void processCommands(const std::string &command, std::map<std::string, nts::Component *> cList)
+int processCommands(const std::string &command, std::map<std::string, nts::Component *> cList)
 {
-	std::map<const std::string, std::function<void(const std::string &, std::map<std::string, nts::Component *>)>> fc;
+	std::map<const std::string, std::function<int(const std::string &, std::map<std::string, nts::Component *>)>> fc;
 
 	fc["exit"] = my_exit;
 	fc["simulate"] = simulate;
@@ -97,9 +112,13 @@ void processCommands(const std::string &command, std::map<std::string, nts::Comp
 	fc["dump"] = dump;
 
 	createSignal();
-	if (fc.find(command) != fc.end())
-		fc[command](command, cList);
-	else if (command.find('=') != std::string::npos)
-		changeInputValue(splitString(command, '='), cList);
-
+	if (fc.find(command) != fc.end()) {
+		if (fc[command](command, cList) == 84)
+			return 84;
+	}
+	else if (command.find('=') != std::string::npos) {
+		if (changeInputValue(splitString(command, '='), cList) == 84)
+			return 84;
+	}
+	return 0;
 }
